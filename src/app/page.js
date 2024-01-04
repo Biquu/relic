92e380151596754080ff4c71ae8cee034448f2a5
MekaRaptor <106775708+MekaRaptor@users.noreply.github.com>
2024-01-05 00:01:11 +0300

    zort
    zattiri

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="bg-white flex min-h-screen flex-col items-center justify-between p-24 text-black">
       <h1>zort</h1>
 
-      {/* Add the ChatBot component here */}
+   
       <ChatBot
         steps={[
           {
@@ -24,8 +24,33 @@ export default function Home() {
           {
             id: '3',
             message: 'Hi {previousValue}, nice to meet you!',
+            trigger: '4',
+          },
+          {
+            id: '4',
+            message: 'What number I am thinking?',
+            trigger: '5',
+          },
+          {
+            id: '5',
+            options: [
+              { value: 1, label: 'Number 1', trigger: '6' },
+              { value: 2, label: 'Number 2', trigger: '7' },
+              { value: 3, label: 'Number 3', trigger: '7' },
+            ],
+          },
+          {
+            id: '7',
+            message: 'Wrong answer, try again.',
+            trigger: '5',
+          },
+          {
+            id: '6',
+            message: 'Awesome! You are a telepath!',
             end: true,
           },
+
+
         ]}
         floating={true}
       />
