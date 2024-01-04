a4ce6b3edbd22c143196dd3743c3842111595459
MekaRaptor <106775708+MekaRaptor@users.noreply.github.com>
2024-01-04 22:15:42 +0300

    chatbot
    chatbot düzenlemesi kaldı bir de anlamsız uyarılar var

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,34 @@
 'use client'
+import React from 'react';
+import ChatBot from 'react-simple-chatbot';
 
-
-
+// Define the Home component
 export default function Home() {
-
-
-
-  
   return (
     <main className="bg-white flex min-h-screen flex-col items-center justify-between p-24 text-black">
       <h1>zort</h1>
+
+      {/* Add the ChatBot component here */}
+      <ChatBot
+        steps={[
+          {
+            id: '1',
+            message: 'What is your name?',
+            trigger: '2',
+          },
+          {
+            id: '2',
+            user: true,
+            trigger: '3',
+          },
+          {
+            id: '3',
+            message: 'Hi {previousValue}, nice to meet you!',
+            end: true,
+          },
+        ]}
+        floating={true}
+      />
     </main>
-  )
+  );
 }
