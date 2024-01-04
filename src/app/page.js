528149ab5541fe7b4a63d54723d480c14d6cfcc3
MekaRaptor <106775708+MekaRaptor@users.noreply.github.com>
2024-01-05 02:18:20 +0300

    Yeni Chat Bot
    Ana Sayfayı Görünce Korkma geçici css o şimdilik bu chatbot da sıkıntı yok ama daha gelişecek

--- src/app/page.js
+++ src/app/page.js
@@ -1,59 +1,23 @@
 'use client'
-import React from 'react';
-import ChatBot from 'react-simple-chatbot';
+import { Chatbot } from 'react-chatbot-kit'; // Make sure to use the correct import
 
+import config from './ChatBot/config';
+import MessageParser from './ChatBot/MessageParser';
+import ActionProvider from './ChatBot/ActionProvider';
+
+import 'react-chatbot-kit/build/main.css';
+import './Home.css'
 // Define the Home component
-export default function Home() {
+function Home() {
   return (
-    <main className="bg-white flex min-h-screen flex-col items-center justify-between p-24 text-black">
-      <h1>zort</h1>
-
-   
-      <ChatBot
-        steps={[
-          {
-            id: '1',
-            message: 'What is your name?',
-            trigger: '2',
-          },
-          {
-            id: '2',
-            user: true,
-            trigger: '3',
-          },
-          {
-            id: '3',
-            message: 'Hi {previousValue}, nice to meet you!',
-            trigger: '4',
-          },
-          {
-            id: '4',
-            message: 'What number I am thinking?',
-            trigger: '5',
-          },
-          {
-            id: '5',
-            options: [
-              { value: 1, label: 'Number 1', trigger: '6' },
-              { value: 2, label: 'Number 2', trigger: '7' },
-              { value: 3, label: 'Number 3', trigger: '7' },
-            ],
-          },
-          {
-            id: '7',
-            message: 'Wrong answer, try again.',
-            trigger: '5',
-          },
-          {
-            id: '6',
-            message: 'Awesome! You are a telepath!',
-            end: true,
-          },
-
-
-        ]}
-        floating={true}
+    <div className='Home'>
+      <Chatbot
+        config={config}
+        messageParser={MessageParser} // Correct the prop name
+        actionProvider={ActionProvider} // Correct the prop name
       />
-    </main>
+    </div>
   );
 }
+
+export default Home;
